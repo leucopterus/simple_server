a85b82dfffe50a802a295bd25093bfe8ab266508
Ed <edgar-str@mail.ru>
2019-08-07 11:05:28 +0300

    sollution for the second task

--- myserver.py
+++ myserver.py
@@ -7,17 +7,26 @@ from http.server import (BaseHTTPRequestHandler,
 from urllib.parse import parse_qs
 
 
+SERVER_ADDRESSES = {1: '127.0.0.1:8001', 2: '127.0.0.2:8002'}
+
+
 class HttpProcessor(BaseHTTPRequestHandler):
+    URLS = {
+        'HOME': '/',
+        'FORM': '/form',
+    }
+
     DEFAULT_ROUTING = {
-        '/': '/'.join([os.getcwd(), 'form.html']),
-        '/form': '/'.join([os.getcwd(), 'form.html']),
+        URLS['HOME']: '/'.join([os.getcwd(), 'form.html']),
+        URLS['FORM']: '/'.join([os.getcwd(), 'form.html']),
     }
 
     def do_GET(self):
         self.routing()
         self.fill_header()
         self.path = self.DEFAULT_ROUTING[self.path]
-        self.rendering()
+        payload = self.context()
+        self.rendering_with_params(**payload)
 
     def routing(self):
         if self.path not in self.DEFAULT_ROUTING:
@@ -29,15 +38,29 @@ class HttpProcessor(BaseHTTPRequestHandler):
         self.send_header('content-type', 'text/html')
         self.end_headers()
 
-    def rendering(self):
+    def context(self, **kwargs):
+        default = {
+            '{{server_a}}': SERVER_ADDRESSES[1],
+            '{{server_b}}': SERVER_ADDRESSES[2],
+        }
+        for key, value in kwargs.items():
+            default[key] = value
+        return default
+
+    def rendering_with_params(self, **kwargs):
         with open(self.path, encoding='UTF-8') as page:
-            self.wfile.write(page.read().encode(encoding='UTF-8'))
-        return
+            full_data = []
+            for line in page:
+                for key, value in kwargs.items():
+                    if line.find(key) != -1:
+                        line = line.replace(key, value)
+                full_data.append(line)
+            data = ''.join(full_data)
+            self.wfile.write(data.encode(encoding="UTF-8"))
 
 
 if __name__ == "__main__":
-    server_addresses = {1: '127.0.0.1:8001', 2: '127.0.0.2:8002'}
-    server_config = server_addresses[1]
+    server_config = SERVER_ADDRESSES[1]
     server_addr, server_port = server_config.split(':')
     try:
         my_server = HTTPServer((server_addr, int(server_port)), HttpProcessor)
--- myserver2.py
+++ myserver2.py
@@ -7,25 +7,35 @@ from http.server import (BaseHTTPRequestHandler,
 from urllib.parse import parse_qs
 
 
+SERVER_ADDRESSES = {1: '127.0.0.1:8001', 2: '127.0.0.2:8002'}
+
+
 class HttpProcessor(BaseHTTPRequestHandler):
-    LOGOUT = '/out'
+    URLS = {
+        'HOME': '/',
+        'AUTH': '/auth',
+        'CHARGE': '/charge',
+        'LOGOUT': '/out',
+    }
+
     DEFAULT_ROUTING = {
-        '/': '/'.join([os.getcwd(), 'auth.html']),
-        '/auth': '/'.join([os.getcwd(), 'auth.html']),
-        '/charge': '/'.join([os.getcwd(), 'charge.html']),
-        LOGOUT: '/'.join([os.getcwd(), 'auth.html']),
+        URLS['HOME']: '/'.join([os.getcwd(), 'auth.html']),
+        URLS['AUTH']: '/'.join([os.getcwd(), 'auth.html']),
+        URLS['CHARGE']: '/'.join([os.getcwd(), 'charge.html']),
+        URLS['LOGOUT']: '/'.join([os.getcwd(), 'auth.html']),
     }
 
     def do_GET(self):
         self.routing()
         self.fill_header()
         self.path = self.DEFAULT_ROUTING[self.path]
-        self.rendering()
+        payload = self.context()
+        self.rendering_with_params(**payload)
 
     def do_POST(self):
         self.routing()
         self.fill_header()
-        if self.path == '/charge':
+        if self.path == self.URLS['CHARGE']:
             form = cgi.FieldStorage(
                 fp=self.rfile,
                 headers=self.headers,
@@ -33,15 +43,12 @@ class HttpProcessor(BaseHTTPRequestHandler):
             )
             purchase = form.getvalue("purchase")
             self.path = self.DEFAULT_ROUTING[self.path]
-            with open(self.path, encoding='UTF-8') as page:
-                full_data = []
-                for line in page:
-                    if '{{from_form.purchase}}' in line:
-                        line = line.replace('{{from_form.purchase}}', str(purchase))
-                    full_data.append(line)
-                data = ''.join(full_data)
-                self.wfile.write(data.encode(encoding="UTF-8"))
-            return
+            payload = {
+                '{{from_form.purchase}}': purchase,
+            }
+            payload = self.context(**payload)
+            self.rendering_with_params(**payload)
+        return
 
     def routing(self):
         if self.path not in self.DEFAULT_ROUTING:
@@ -51,11 +58,11 @@ class HttpProcessor(BaseHTTPRequestHandler):
 
     def fill_header(self):
         self.send_header('content-type', 'text/html')
-        if self.path in ('/', '/auth'):
+        if self.path in (self.URLS['HOME'], self.URLS['AUTH']):
             self.handle_auth("OK")
-        elif self.path == '/charge':
+        elif self.path == self.URLS['CHARGE']:
             self.handle_charge()
-        elif self.path == self.LOGOUT:
+        elif self.path == self.URLS['LOGOUT']:
             self.handle_auth()
         self.end_headers()
 
@@ -65,19 +72,32 @@ class HttpProcessor(BaseHTTPRequestHandler):
         self.send_header("Set-Cookie", cookie.output(header='', sep=''))
 
     def handle_charge(self):
-        # pass cookies from one server to another
         if not self.headers.get("cookie") or self.headers.get("cookie") != 'auth_cookie=OK':
             self.send_error(403, message="Forbidden")
 
-    def rendering(self):
+    def context(self, **kwargs):
+        default = {
+            '{{server_a}}': SERVER_ADDRESSES[1],
+            '{{server_b}}': SERVER_ADDRESSES[2],
+        }
+        for key, value in kwargs.items():
+            default[key] = value
+        return default
+
+    def rendering_with_params(self, **kwargs):
         with open(self.path, encoding='UTF-8') as page:
-            self.wfile.write(page.read().encode(encoding='UTF-8'))
-        return
+            full_data = []
+            for line in page:
+                for key, value in kwargs.items():
+                    if line.find(key) != -1:
+                        line = line.replace(key, value)
+                full_data.append(line)
+            data = ''.join(full_data)
+            self.wfile.write(data.encode(encoding="UTF-8"))
 
 
 if __name__ == "__main__":
-    server_addresses = {1: '127.0.0.1:8001', 2: '127.0.0.2:8002'}
-    server_config = server_addresses[2]
+    server_config = SERVER_ADDRESSES[2]
     server_addr, server_port = server_config.split(':')
     try:
         my_server = HTTPServer((server_addr, int(server_port)), HttpProcessor)
