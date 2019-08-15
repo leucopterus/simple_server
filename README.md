cdcb071ff864be0b94c0d28c082ef67545f442db
Ed <edgar-str@mail.ru>
2019-08-15 16:50:26 +0300

    changes were made due to requirements

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-Two servers were created. They are launched on different ip addresses.
-The first server has and processes only one html page - form.html.
-Another one has auth and charge pages. In the first server's auth page cookies is set.
-Auth cookie set OK when auth page is visited, to reset cookie just push 'to reset cookie'
-link.
+Two server was created. With the help of argparse it is possible to run two
+different servers using just one script.
+HTML pages are common but divided, the first server has auth.hml and charge.html
+ pages.
+Another one has just one form.html page. In the first server's auth page 
+cookies are set.
+Auth cookie set OK when auth page is visited, to delete cookie just push 
+'to reset cookie' link.  
 HOW TO run task:
-1) download all files at the same directory
-2) in the first terminal run myserver.py python script
-3) in the second terminal run myserver2.py python script
-4) visit 127.0.0.2:8002 page
+0. default ip addresses: `127.0.0.1:8001` and `0.0.0.0:8002`. To change them you should
+write the one by one in terminal, then in the second
+ terminal write them in reverse order. If you want to use defaults: 
+ use `-r` key for reverse (will be shown at the next steps)
+1. download all files at the same directory
+2. in the first terminal run `python3 myserver.py`
+3. in the second terminal run `python3 myserver.py -r`
+4. visit [127.0.0.1:8001](http://127.0.0.1:8001) page
--- myserver.py
+++ myserver.py
@@ -1,68 +1,172 @@
 import os
+import cgi
+import datetime
+import argparse
 
+from http import cookies
 from http.server import (BaseHTTPRequestHandler,
                          HTTPServer)
 
 
-SERVER_ADDRESSES = {1: '127.0.0.1:8001', 2: '127.0.0.2:8002'}
+SERVER_ADDRESSES = {1: '127.0.0.1:8001', 2: '0.0.0.0:8002'}
 
 
 class HttpProcessor(BaseHTTPRequestHandler):
-    # allowed urls
+    # list of allowed urls
     URLS = {
         'HOME': '/',
+        'AUTH': '/auth',
         'FORM': '/form',
+        'CHARGE': '/charge',
+        'LOGOUT': '/out',
     }
 
-    # url and path connection
+    # connect url and html file in the PC
     DEFAULT_ROUTING = {
-        URLS['HOME']: '/'.join([os.getcwd(), 'form.html']),
+        URLS['HOME']: '/'.join([os.getcwd(), 'auth.html']),
+        URLS['AUTH']: '/'.join([os.getcwd(), 'auth.html']),
         URLS['FORM']: '/'.join([os.getcwd(), 'form.html']),
+        URLS['CHARGE']: '/'.join([os.getcwd(), 'charge.html']),
+        URLS['LOGOUT']: '/'.join([os.getcwd(), 'auth.html']),
     }
 
     def do_GET(self) -> None:
-        self.routing()
-        self.fill_header()
-        self.path = self.DEFAULT_ROUTING[self.path]
-        payload = self.context()
-        self.rendering_with_params(**payload)
+        self.verify_url()
+        self.file_path = self.DEFAULT_ROUTING[self.path]
+        if os.path.exists(self.file_path):
+            self.send_response(200)
+            self.fill_header()
+            payload = self.context()
+            data = self.rendering_with_params(**payload)
+            self.wfile.write(data.encode(encoding="UTF-8"))
+        else:
+            self.send_error(500)
 
-    def routing(self) -> None:
+    def do_POST(self) -> None:
+        self.verify_url()
+        if self.path == '/charge':
+            form = cgi.FieldStorage(
+                fp=self.rfile,
+                headers=self.headers,
+                environ={'REQUEST_METHOD': 'POST'}
+            )
+            purchase = form.getvalue("purchase")
+            if purchase is None:
+                self.send_error(400)
+            try:
+                int(purchase)
+            except ValueError:
+                try:
+                    float(purchase)
+                except ValueError:
+                    self.send_error(400)
+            self.file_path = self.DEFAULT_ROUTING[self.path]
+            if not os.path.exists(self.file_path):
+                self.send_error(500)
+            payload = {
+                '{purchase}': purchase,
+            }
+            payload = self.context(**payload)
+            data = self.rendering_with_params(**payload)
+            self.send_response(200)
+            self.fill_header()
+            self.wfile.write(data.encode(encoding="UTF-8"))
+        else:
+            self.send_error(404)
+
+    def verify_url(self) -> None:
         """check if the page is presented"""
         if self.path not in self.DEFAULT_ROUTING:
-            self.send_error(404, message="Page Not Found")
-        else:
-            self.send_response(200, message="OK")
+            self.send_error(404)
 
     def fill_header(self) -> None:
-        """set headers"""
+        """
+        add or delete cookies when login or logout page is chosen
+        check if cookies are presented to get access to charge page
+        """
         self.send_header('content-type', 'text/html')
+        if self.path in (self.URLS['HOME'], self.URLS['AUTH']):
+            self.handle_auth("OK")
+        elif self.path == self.URLS['CHARGE']:
+            self.handle_charge_access()
+        elif self.path == self.URLS['LOGOUT']:
+            self.handle_auth()
         self.end_headers()
 
+    def handle_auth(self, auth_val=0) -> None:
+        """set or delete cookie"""
+        cookie = cookies.SimpleCookie()
+        cookie["auth_cookie"] = auth_val
+        if not auth_val:
+            expires = datetime.datetime(1970, 1, 1, 0, 0, 0)
+            cookie["auth_cookie"]["expires"] = expires.strftime('%a, %d %b %Y %H:%M:%S')
+        self.send_header("Set-Cookie", cookie.output(header='', sep=''))
+
+    def handle_charge_access(self) -> None:
+        """check cookie"""
+        if not self.headers.get("cookie") or self.headers.get("cookie") != 'auth_cookie=OK':
+            self.send_error(401, message="Forbidden")
+
     def context(self, **kwargs) -> dict:
-        """set default context to change in html pages"""
+        """create full dict of values to change in html"""
         default = {
-            '{{server_a}}': SERVER_ADDRESSES[1],
-            '{{server_b}}': SERVER_ADDRESSES[2],
+            '{server_a}': SERVER_ADDRESSES[1],
+            '{server_b}': SERVER_ADDRESSES[2],
         }
         for key, value in kwargs.items():
             default[key] = value
         return default
 
-    def rendering_with_params(self, **kwargs) -> None:
+    def rendering_with_params(self, **kwargs) -> str:
         """rendering page with parameters (like Jinja)"""
-        with open(self.path, encoding='UTF-8') as page:
+        with open(self.file_path, encoding='UTF-8') as page:
             full_data = []
             for line in page:
                 for key, value in kwargs.items():
                     if line.find(key) != -1:
                         line = line.replace(key, value)
                 full_data.append(line)
-            data = ''.join(full_data)
-            self.wfile.write(data.encode(encoding="UTF-8"))
+        data = ''.join(full_data)
+        if not data:
+            self.send_error(400)
+        return data
 
 
-if __name__ == "__main__":
+def init_parser():
+    """parser to run servers using one script"""
+    parser = argparse.ArgumentParser(
+        prog="Server",
+        prefix_chars='-',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Program to work with simple server")
+    parser.add_argument(
+        "serverA",
+        nargs='?',
+        type=str)
+    parser.add_argument(
+        "serverB",
+        nargs='?',
+        type=str)
+    parser.add_argument(
+        "-r", "--reverse",
+        action="store_true",
+        default=False,
+        help="Reverse both servers' ips"
+    )
+    return parser
+
+
+if __name__ == '__main__':
+    parser = init_parser()
+    args = parser.parse_args()
+    if args.reverse:
+        SERVER_ADDRESSES[1], SERVER_ADDRESSES[2] = SERVER_ADDRESSES[2], SERVER_ADDRESSES[1]
+    else:
+        if args.serverA:
+            SERVER_ADDRESSES[1] = args.serverA
+        if args.serverB:
+            SERVER_ADDRESSES[2] = args.serverB
+    print(f'Your server addresses are: {SERVER_ADDRESSES[1]} and {SERVER_ADDRESSES[2]}')
     server_config = SERVER_ADDRESSES[1]
     server_addr, server_port = server_config.split(':')
     my_server = HTTPServer((server_addr, int(server_port)), HttpProcessor)
