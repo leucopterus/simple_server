b4b5c2df8ebd40a6526e2ff659ca0ddede22b046
Ed <edgar-str@mail.ru>
2019-08-16 15:21:22 +0300

    changes due to reqirements

--- myserver.py
+++ myserver.py
@@ -1,6 +1,7 @@
 import os
 import cgi
 import datetime
+import decimal
 
 from http import cookies
 from http.server import (BaseHTTPRequestHandler,
@@ -8,59 +9,67 @@ from http.server import (BaseHTTPRequestHandler,
 
 
 class HttpProcessor(BaseHTTPRequestHandler):
+
     LOGOUT = '/out'
+    CHARGE = '/charge'
+    FORM = '/form'
+    HOME = '/'
+    AUTH = '/auth'
+
+    LIST_OF_POST_METHOD_PAGES = [CHARGE]
+
     DEFAULT_ROUTING = {
-        '/': os.path.join(os.getcwd(), 'auth.html'),
-        '/auth': os.path.join(os.getcwd(), 'auth.html'),
-        '/form': os.path.join(os.getcwd(), 'form.html'),
-        '/charge': os.path.join(os.getcwd(), 'charge.html'),
+        HOME: os.path.join(os.getcwd(), 'auth.html'),
+        AUTH: os.path.join(os.getcwd(), 'auth.html'),
+        FORM: os.path.join(os.getcwd(), 'form.html'),
+        CHARGE: os.path.join(os.getcwd(), 'charge.html'),
         LOGOUT: os.path.join(os.getcwd(), 'auth.html'),
     }
 
-    def do_GET(self):
+    def do_GET(self) -> None:
+        """GET method implementation"""
         self.verify_url()
         self.file_path = self.DEFAULT_ROUTING[self.path]
-        if os.path.exists(self.file_path):
-            self.send_response(200)
-            self.fill_header()
-            self.rendering()
-        self.send_error(500)
+        self.check_file_existence()
+        self.send_response(200)
+        self.fill_header()
+        self.rendering()
 
-    def do_POST(self):
+    def do_POST(self) -> None:
+        """POST method implementation"""
         self.verify_url()
-        if self.path == '/charge':
+        if self.path in self.LIST_OF_POST_METHOD_PAGES:
             form = cgi.FieldStorage(
                 fp=self.rfile,
                 headers=self.headers,
                 environ={'REQUEST_METHOD': 'POST'}
             )
-            purchase = form.getvalue("purchase")
-            if purchase is None:
-                self.send_error(400)
-            try:
-                int(purchase)
-            except ValueError:
-                try:
-                    float(purchase)
-                except ValueError:
-                    self.send_error(400)
+            form_data = ''
+            html_page = ''
+            if self.path == self.CHARGE:
+                form_data = self.process_charge_data(form)
             self.file_path = self.DEFAULT_ROUTING[self.path]
-            if not os.path.exists(self.file_path):
-                self.send_error(500)
-            with open(self.file_path, encoding='UTF-8') as page:
-                data = page.read()
-                data = data.format(purchase=purchase)
+            self.check_file_existence()
+            if self.path == self.CHARGE:
+                html_page = self.rendering_charge(form_data)
             self.send_response(200)
             self.fill_header()
-            self.wfile.write(data.encode(encoding="UTF-8"))
+            self.wfile.write(html_page.encode(encoding="UTF-8"))
         else:
-            self.send_error(404)
+            self.send_error(404, message="Page Not Found")
 
-    def verify_url(self):
+    def verify_url(self) -> None:
+        """Check if page on server is presented"""
         if self.path not in self.DEFAULT_ROUTING:
             self.send_error(404, message="Page Not Found")
 
-    def fill_header(self):
+    def check_file_existence(self) -> None:
+        """Check is file in a dirictory"""
+        if not os.path.exists(self.file_path):
+            self.send_error(500)
+
+    def fill_header(self) -> None:
+        """Fill out response header"""
         self.send_header('content-type', 'text/html')
         if self.path in ('/', '/auth'):
             self.handle_auth("OK")
@@ -70,7 +79,8 @@ class HttpProcessor(BaseHTTPRequestHandler):
             self.handle_auth()
         self.end_headers()
 
-    def handle_auth(self, auth_val=0):
+    def handle_auth(self, auth_val=0) -> None:
+        """Set or delete cookie for auth page"""
         cookie = cookies.SimpleCookie()
         cookie["auth_cookie"] = auth_val
         if not auth_val:
@@ -78,13 +88,33 @@ class HttpProcessor(BaseHTTPRequestHandler):
             cookie["auth_cookie"]["expires"] = expires.strftime('%a, %d %b %Y %H:%M:%S')
         self.send_header("Set-Cookie", cookie.output(header='', sep=''))
 
-    def handle_charge_access(self):
+    def handle_charge_access(self) -> None:
+        """Check cookie to access the charge page"""
         if not self.headers.get("cookie") or self.headers.get("cookie") != 'auth_cookie=OK':
             self.send_error(401, message="Forbidden")
 
-    def rendering(self):
+    def process_charge_data(self, form) -> str:
+        """Collect date out of form"""
+        purchase = form.getvalue("purchase")
+        if purchase is None:
+            self.send_error(400, message='invalid sum value, should be number')
+        try:
+            decimal.Decimal(purchase)
+        except decimal.InvalidOperation:
+            self.send_error(400, message='invalid sum value, should be number')
+        return purchase
+
+    def rendering(self) -> None:
+        """Collect data from html file without parameters"""
         with open(self.file_path, encoding='UTF-8') as page:
             self.wfile.write(page.read().encode(encoding='UTF-8'))
+
+    def rendering_charge(self, purchase) -> str:
+        """Collect data from html file with parameters"""
+        with open(self.file_path, encoding='UTF-8') as page:
+            data = page.read()
+            data = data.format(purchase=purchase)
+        return data
 
 
 if __name__ == '__main__':
